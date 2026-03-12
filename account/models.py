01d9a8a9ad63aec53cc645397f9a30dba05e4bf6
Matio004 <mateuszkupis@interia.eu>
2026-03-12 21:25:40 +0100

    Add markdown filter

--- account/models.py
+++ account/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    photo = models.ImageField()
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
 
     def __str__(self):
         return f'Profile of user {self.user.username}'
