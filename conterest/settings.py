a66dc732cfc52dbbc190e388b9e81969c509975c
Matio004 <mateuszkupis@interia.eu>
2026-03-14 17:35:01 +0100

    Make website pretty

--- conterest/settings.py
+++ conterest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!=xozgng*h19w)&s%vg$vz&uv%19hex&i5ki@z5^bt^o8$22x+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
